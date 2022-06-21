e23b09c778a16d00cbbc94419534e0a52c5a3c29
Daniele Gargiulo <daniele.gargiulo@codiceplastico.com>
2022-06-21 11:46:47 +0200

    feat: highlight selected header link

--- src/app/shared/components/header/Header.tsx
+++ src/app/shared/components/header/Header.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 import './Header.scss';
 
 export default function Header() {
 	const links = [
 		{ label: 'Home', href: '/' },
 		{ label: 'About', href: '/about' },
-		{ label: 'Projects', href: '/about' },
+		{ label: 'Projects', href: '/projects' },
 		{ label: 'Contact', href: '/contact' },
 		// { label: 'Services', href: '/services' },
 	];
@@ -30,13 +30,22 @@ export default function Header() {
 			</span>
 			<ul>
 				{links.map((link) => {
-					return (
-						<li>
-							<Link to={link.href}>{link.label}</Link>
-						</li>
-					);
+					return <HeaderLink key={link.href} label={link.label} href={link.href} />;
 				})}
 			</ul>
 		</header>
+	);
+}
+
+function HeaderLink({ label, href }: { label: string; href: string }) {
+	const resolvedPath = useResolvedPath(href);
+	const isActive = useMatch({ path: resolvedPath.pathname });
+
+	return (
+		<li>
+			<Link className={isActive ? 'active' : ''} to={href}>
+				{label}
+			</Link>
+		</li>
 	);
 }
